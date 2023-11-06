2694e7e4f098e1e024a83c6b7076228ecea68af9
akihiko47 <akihiko4701a@gmail.com>
2023-11-06 21:59:11 +0300

    First Demo

---
+++ index.js
@@ -0,0 +1,88 @@
+// start only when everything is loaded
+window.onload = function () {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+
+  let particles = []; // main array of particles
+  let springs = []; // main array of springs
+  let joints = []; // main array of rigid joints
+
+  // update function iterations
+  // more substeps => better accuracy
+  // more substeps => poor performance
+  let subSteps = 4;
+
+  let G = 10; // gravitational force
+
+  let secNow = 0; // variable for storing frame start time
+  let secPrev = 0; // variable for storing the end time of the previous frame
+  let secPassed = 0; // time betweeen frames
+
+  // MAIN FUNCTION
+  function main() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    secNow = window.performance.now(); // get start time of this frame
+
+    // substeps loop
+    // you can perform several updates per frame for greater accuracy
+    // in this case, the frame time is divided by the number of sub-steps
+    for (let i = 0; i < subSteps; i++) {
+      applyGravity(particles, G);
+      update(particles, springs, joints, secPassed / subSteps);
+      drawParticles(particles);
+
+      // this function prevents particles from falling off the screen
+      applyConstraint(particles, width, height);
+    }
+
+    secPassed = (secNow - secPrev) / 1000; // get frame time and convers to seconds
+    secPrev = secNow; // get end time of this frame
+
+    requestAnimationFrame(main); // continue main loop
+  }
+
+  function addRect() {
+        
+    // add rectangle vertices
+    // particle => x, y, radius(default=0), color(default=white), anchored(default=false)
+    // add particle to particles list to update and draw
+    particles.push(new Particle(500, 700, 15));
+    particles.push(new Particle(500, 500, 15));
+    particles.push(new Particle(700, 500, 15));
+    particles.push(new Particle(700, 700, 15));
+
+    // add rectangle edges
+    // spring => particle1, particle2, strength, damping, width(default=0) color(default=white)
+    // add spring to springs list to update and draw
+    springs.push(new Spring(particles[0], particles[1], 0.8, 0.5, 5));
+    springs.push(new Spring(particles[1], particles[2], 0.8, 0.5, 5));
+    springs.push(new Spring(particles[2], particles[3], 0.8, 0.5, 5));
+    springs.push(new Spring(particles[3], particles[0], 0.8, 0.5, 5));
+    springs.push(new Spring(particles[0], particles[2], 0.8, 0.5, 5));
+    springs.push(new Spring(particles[3], particles[1], 0.8, 0.5, 5));
+  }
+
+  function addRope() {
+    // add anchor
+    // particle => x, y, radius(default=0), color(default=white), anchored(default=false)
+    particles.push(new Particle(900, 500, 15, undefined, true))
+
+    // add 15 rope particles
+    // particle => x, y, radius(default=0), color(default=white), anchored(default=false)
+    for (let i = 0; i < 15; i++) {
+        particles.push(new Particle(935 + (35 + i) * i, 500, 15 + i))
+    }
+
+    // add rigid joints between rope particles
+    // joint => particle1, particle2, width(default=0) color(default=white)
+    for (let i = 0; i < 15; i++) {
+        joints.push(new Joint(particles[i+4], particles[i+5]))
+    }
+  }
+
+  addRect();
+  addRope();
+  main();
+};
---
+++ physics/utils.js
@@ -0,0 +1,116 @@
+
+
+function drawParticles(particles) {
+    for (let particle of particles) {
+        if (particle.radius) {
+            particle.domElement.style.width = particle.radius * 2 + "px";
+            particle.domElement.style.height = particle.radius * 2 + "px";
+            particle.domElement.style.left = particle.x_now - particle.radius + "px";
+            particle.domElement.style.top = particle.y_now - particle.radius + "px";
+
+        }
+    }
+}
+
+// function drawSprings(content, springs) {
+//     for (let spring of springs) {
+//         if (spring.width) {
+//             content.beginPath();
+//             content.moveTo(spring.p1.x_now, spring.p1.y_now);
+//             content.lineTo(spring.p2.x_now, spring.p2.y_now);
+//             content.lineWidth = spring.width;
+//             content.strokeStyle = spring.color;
+//             content.stroke();
+//         }
+//     }
+// }
+
+// function drawJoints(content, joints) {
+//     for (let joint of joints) {
+//         if (joint.width) {
+//             content.beginPath();
+//             content.moveTo(joint.p1.x_now, joint.p1.y_now);
+//             content.lineTo(joint.p2.x_now, joint.p2.y_now);
+//             content.lineWidth = joint.width;
+//             content.strokeStyle = joint.color;
+//             content.stroke();
+//         }
+//     }
+// }
+
+function applyGravity(particles, G) {
+    for (let particle of particles) {
+        particle.accelerate(0, G * 100);
+	}
+}
+
+function update(particles, springs, joints, dt) {
+    updatePositions(particles, dt);
+    handleBetweenCollision(particles);
+    applySprings(springs);
+    applyJoints(joints);
+}
+
+function applySprings(springs) {
+    for (let spring of springs) {
+        spring.applyStringForce();
+    }
+}
+
+function applyJoints(joints) {
+    for (let joint of joints) {
+        joint.applyJoint();
+    }
+}
+
+function updatePositions(particles, dt) {
+    for (let particle of particles) {
+        if (!particle.anchored) {
+            particle.update(dt);
+        } else {
+            particle.updateAnchor();
+        }
+    }
+}
+
+function applyConstraint(particles, width, height) {
+    for (let particle of particles) {
+        if (particle.x_now + particle.radius > width) {
+            particle.x_now = width - particle.radius;
+        }
+        if (particle.x_now - particle.radius < 0) {
+            particle.x_now = particle.radius;
+        }
+        if (particle.y_now + particle.radius > height) {
+            particle.y_now = height - particle.radius;
+        }
+        if (particle.y_now - particle.radius < 0) {
+            particle.y_now = particle.radius;
+        }
+    }
+}
+
+function handleBetweenCollision(particles) {
+    for (let particle1 of particles) {
+        for (let particle2 of particles) {
+            if ((particle1 !== particle2) & (particle1.radius != 0 & particle2.radius != 0)) {
+                let col_axis_x = particle1.x_now - particle2.x_now;
+                let col_axis_y = particle1.y_now - particle2.y_now;
+
+                let dist = Math.sqrt(col_axis_x**2 + col_axis_y**2);
+
+                if (dist < particle1.radius + particle2.radius) {
+                    let n_x = col_axis_x / dist;
+                    let n_y = col_axis_y / dist;
+
+                    let delta = particle1.radius + particle2.radius - dist;
+
+                    particle1.x_now += 0.5 * delta * n_x;
+                    particle1.y_now += 0.5 * delta * n_y;
+                    particle2.x_now -= 0.5 * delta * n_x;
+                    particle2.y_now -= 0.5 * delta * n_y;
+                }
+            }
+        }
+    }
+}
